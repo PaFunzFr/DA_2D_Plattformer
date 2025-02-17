17fb301691b0c3c695f2caa9ff331ad4a4e156a6
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-02-17 16:49:56 +0100

    first declarations

---
+++ script/game.js
@@ -0,0 +1,7 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById("canvas");
+    character.src = "../img/2_character_pepe/1_idle/long_idle/I-11.png";
+}
