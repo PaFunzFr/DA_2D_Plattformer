449de381c90d1e3d3b450c3253154c9e463ad06c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-02-19 09:31:04 +0100

    added jump animation

--- script/models/character.class.js
+++ script/models/character.class.js
@@ -4,56 +4,84 @@ class Character extends MovableObject {
     currentImage = 0;
     imagesWalking = [];
     imagesJumping = [];
+    imagesIdle = [];
     isJumping = false;
     x = 0;
     characterSpeed = 10;
+    jumpHeight = 90
+    speedY = 0;
+    acceleration = 0.5;
+    characterOnGround = 480 - this.height - this.yOffset;
 
     constructor() {
         super();
-        this.y = 480 - this.height - this.yOffset;
+        this.y = this.characterOnGround;
         this.loadImage("../img/2_character_pepe/2_walk/W-1.png");
         this.preloadImages(this.imagesWalking, "../img/2_character_pepe/2_walk/W-", 6);
         this.preloadImages(this.imagesJumping, "../img/2_character_pepe/3_jump/J-", 9);
+        this.preloadImages(this.imagesIdle, "../img/2_character_pepe/1_idle/idle/I-", 10);
+        this.applyGravity();
         this.animate();
     }
 
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY
+                this.speedY -= this.acceleration; 
+                this.isOnGround();
+            }
+        }, 1000/60);
+    }
+
+    isAboveGround() {
+        return this.y + this.yOffset < 180
+    }
+
+    isOnGround() {
+        if (this.y >= this.characterOnGround) {
+            this.y = this.characterOnGround;
+            this.speedY = 0; 
+            this.isJumping = false;
+            this.currentImage = 0; // reset animation
+        }
+    }
+
     animate() {
+        this.isOnGround();
             setInterval(() => {
-                if (!this.isJumping && this.world.keyboard.clickedRight && this.x < this.world.level.levelEndX) {
+                if (this.world.keyboard.clickedRight && this.x < this.world.level.levelEndX) {
                     this.x += this.characterSpeed;
+                    //this.walkingSound.play();
                     this.otherDirection = false;
                 }
-                if (!this.isJumping && this.world.keyboard.clickedLeft && this.x > -719) {
+
+                if (this.world.keyboard.clickedLeft && this.x > -719) {
                     this.x -= this.characterSpeed;
+                    //this.walkingSound.play();
                     this.otherDirection = true;
                 }
+                
+                if (this.world.keyboard.clickedUp && !this.isJumping) {
+                    this.isJumping = true;
+                    this.speedY = 10;
+                }
+
                 this.world.cameraX = -this.x;
             }, 1000/60);
 
+            // ANIMATION SET (PICTURES)
             setInterval(() => {
-                if (!this.isJumping && this.world.keyboard.clickedRight) {
-                    this.playAnimation(this.imagesWalking);
+                if (this.isAboveGround()) {
+                    this.playAnimation(this.imagesJumping);
+                } else {
+                    if (!this.isJumping && this.world.keyboard.clickedRight || this.world.keyboard.clickedLeft) {
+                        this.playAnimation(this.imagesWalking);
+                    } else {
+                        this.playAnimation(this.imagesIdle);
+                    }
                 }
-                if (!this.isJumping && this.world.keyboard.clickedLeft) {
-                    this.playAnimation(this.imagesWalking);
-                }
-            }, 70);
+            }, 100);
     }
 
-    jump() {
-        if (this.isJumping) return;
-        this.isJumping = true;
-        this.currentImage = 0;
-        let jumpInterval = setInterval(() => {
-            this.playAnimation(this.imagesJumping);
-            if (this.currentImage >= this.imagesJumping.length) {
-                this.endJump(jumpInterval);
-            }
-        }, 70);
-    }
-    
-    endJump(interval) {
-        clearInterval(interval);
-        this.isJumping = false;
-    }
 }
